8fb7ebf115a4b498b89a6946aefc9106d1960b0d
Анастасія Кулинич <kulynychnastya@gmail.com>
2022-07-15 18:21:58 +0300

    hw 4

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { userRouter } = require('./routes');
 const { constants } = require('./configs');
+
+mongoose.connect('mongodb://localhost:27017/test');
 
 const app = express();
 app.use(express.json());
@@ -9,7 +12,16 @@ app.use(express.json());
 app.use('/users', userRouter);
 
 app.use('*', (req, res) => {
-    res.status(404).json('Page not found');
+    res.status(404).json('Route not found');
+});
+
+app.use((err, req, res, next) => {
+    res
+        .status(err.status || 500)
+        .json({
+            error: err.message || 'Unknown Error',
+            code: err.status || 500
+        });
 });
 
 app.listen(constants.PORT, () => {
